5cd8ac146744e7243962679d23c2506a1b073afd
nicobrch <nicolas.brch@gmail.com>
2023-04-17 22:18:03 -0400

    Modificaciones para N jugadores y seguir jugando hasta que el monto sea < 0 o se retire

--- Tarea 1/mesa.cpp
+++ Tarea 1/mesa.cpp
@@ -13,6 +13,7 @@ int main() {
 
     int fd1[cantidadJugadores], fd2[cantidadJugadores];
     sem_t *mutex[cantidadJugadores];
+    Jugador jugadores[cantidadJugadores];
 
     //  Crear pipes
     for (i=0; i < (cantidadJugadores*2); i++)
@@ -32,90 +33,119 @@ int main() {
         contador++;
     }
 
-    // Crear semaforos
+    //  Crear semaforos
     for (i=0; i < cantidadJugadores; i++)
     {
         string path = "mutex" + to_string(i);
         mutex[i] = sem_open(path.c_str(), O_CREAT, 0666, 1);
     }
 
+    //  Esperar que los jugadores se conecten
     cout << "Esperando jugadores..." << endl;
 
-    //  Leer mensaje desde la PIPE1
-    msg = leerMensajeInicial(fd1[0], msg);
+    for (i=0; i<cantidadJugadores; i++){
+        //  Leer mensaje desde la PIPE1
+        msg = leerMensajeInicial(fd1[i], msg);
 
-    //  Crear jugador que se ha conectado
-    auto jugador1 = new Jugador(msg.process_id, 1000);
+        //  Crear jugador que se ha conectado
+        jugadores[i].setId(msg.process_id);
+        jugadores[i].setMonto(1000);
+        jugadores[i].agregarCarta();
+        jugadores[i].agregarCarta();
 
-    cout << "fd2[0]: " << fd2[0] << endl;
-    //  Enviar mensaje a la PIPE2
-    enviarMensaje(fd2[0], pid, mutex[0], msg, "Hola Jugador " + to_string(jugador1->getId()) + "! Bienvenido al" +
-        " juego del 21.\nUsted cuenta con un monto total de " + to_string(jugador1->getMonto()) + ". Por favor " +
-        "indique cuánto desea apostar.");
+        //  Enviar mensaje a la PIPE2
+        enviarMensaje(fd2[i], pid, mutex[i], msg,
+    "Hola Jugador " + to_string(jugadores[i].getId()) + "! Bienvenido al juego del 21" +
+            "\nUsted cuenta con un monto total de " + to_string(jugadores[i].getMonto()) +
+            ".\nPor favor indique cuánto desea apostar.");
 
-    //  Leer mensaje desde la PIPE1
-    msg = leerMensaje(fd1[0], mutex[0], msg);
+        //  Leer mensaje desde la PIPE1
+        msg = leerMensaje(fd1[i], mutex[i], msg);
 
-    //  Modificar monto
-    int apuesta = charArrayToInt(msg);
-    auxMsj = jugador1->imprimirCartas();
+        //  Modificar monto
+        int apuesta = charArrayToInt(msg);
+        auxMsj = jugadores[i].imprimirCartas();
 
-    //  Enviar cartas sus cartas al jugador por la PIPE2
-    enviarMensaje(fd2[0], pid, mutex[0], msg, auxMsj);
-
-    //  Leer mensaje desde la PIPE1
-    msg = leerMensaje(fd1[0], mutex[0], msg);
-
-    int opcion = charArrayToInt(msg);
-    if (opcion == 1)
-    {
-        //  Desea una carta adicional.
-        jugador1->agregarCarta();
-        auxMsj = jugador1->imprimirCartas();
-        //  Enviar cartas sus cartas al jugador por la PIPE2
-        enviarMensaje(fd2[0], pid, mutex[0], msg, auxMsj);
-    }
-    else
-    {
-        //  Desea apostar con las que ya tiene.
-        int puntajeMesa = mesa->getPuntaje(), puntajeJugador = jugador1->getSumaCartas();
-
-        if (puntajeJugador > puntajeMesa && puntajeJugador <= 21)
+        //  Bucle que consulta por si quiere agregar o no cartas
+        while(jugadores[i].getMonto() > 0)
         {
-            //  Ganó el juego
-            //  Sumar lo apostado a su monto total
-            jugador1->setMonto(jugador1->getMonto() + (apuesta * 2));
-            //  Mensaje ganador
-            auxMsj = "El puntaje de la mesa es de: " + to_string(puntajeMesa) +
-                     "\nEl puntaje del jugador es de: " + to_string(puntajeJugador) +
-                     "\nJugador " + to_string(jugador1->getId()) + " ha ganado!" +
-                     "\nSu nuevo monto es de: " + to_string(jugador1->getMonto()) + "\n";
-            //  Enviar mensaje de victoria por la PIPE2
-            enviarMensaje(fd2[0], pid, mutex[0], msg, auxMsj);
-        }
-        else if (puntajeJugador > 21)
-        {
-            //  Sumar lo apostado a su monto total
-            jugador1->setMonto(jugador1->getMonto() - apuesta);
-            //  Mensaje ganador
-            auxMsj = "El puntaje de la mesa es de: " + to_string(puntajeMesa) +
-                     "\nEl puntaje del jugador es de: " + to_string(puntajeJugador) +
-                     "\nJugador " + to_string(jugador1->getId()) + " ha perdido por pasarse del 21 :(" +
-                     "\nSu nuevo monto es de: " + to_string(jugador1->getMonto()) + "\n";
-            //  Enviar mensaje de victoria por la PIPE2
-            enviarMensaje(fd2[0], pid, mutex[0], msg, auxMsj);
-        }
-        else
-        {
-            //  Sumar lo apostado a su monto total
-            jugador1->setMonto(jugador1->getMonto() - apuesta);
-            //  Mensaje ganador
-            auxMsj = "El puntaje de la mesa es de: " + to_string(puntajeMesa) +
-                     "\nEl puntaje del jugador es de: " + to_string(puntajeJugador) +
-                     "\nJugador " + to_string(jugador1->getId()) + " ha perdido por tener menos puntos que la mesa :(" +
-                     "\nSu nuevo monto es de: " + to_string(jugador1->getMonto()) + "\n";
-            //  Enviar mensaje de victoria por la PIPE2
-            enviarMensaje(fd2[0], pid, mutex[0], msg, auxMsj);
+            //  Variables
+            int puntajeMesa = mesa->getPuntaje(), puntajeJugador = jugadores[i].getSumaCartas();
+
+            //  Enviar sus cartas al jugador por la PIPE2
+            enviarMensaje(fd2[i], pid, mutex[i], msg, auxMsj);
+
+            //  Leer mensaje desde la PIPE1
+            msg = leerMensaje(fd1[i], mutex[i], msg);
+
+            //  Opcion ingresada por el jugador parseada a int
+            int opcion = charArrayToInt(msg);
+
+            if (opcion == 1)
+            {
+                //  Desea una carta adicional.
+                jugadores[i].agregarCarta();
+                if (jugadores[i].getSumaCartas() > 21)
+                {
+                    //  Modificar monto
+                    jugadores[i].setMonto(jugadores->getMonto() - apuesta);
+                    //  Reiniciar cartas para nuevo juego
+                    jugadores[i].nuevoJuego();
+                    mesa->nuevoPuntaje();
+                    //  Mensaje ganador
+                    auxMsj = "El puntaje de la mesa es de: " + to_string(puntajeMesa) +
+                             "\nEl puntaje del jugador es de: " + to_string(puntajeJugador) +
+                             "\n> Jugador " + to_string(jugadores[i].getId()) + " ha perdido por pasarse del 21 :(" +
+                             "\nSu nuevo monto es de: " + to_string(jugadores[i].getMonto()) +
+                             "\n[---- NUEVO JUEGO ----]\n";
+                    auxMsj += jugadores[i].imprimirCartas();
+                }
+                else
+                {
+                    auxMsj = jugadores[i].imprimirCartas();
+                }
+            }
+            else
+            {
+                if (puntajeJugador > puntajeMesa && puntajeJugador <= 21)
+                {
+                    //  Ganó el juego
+                    //  Sumar lo apostado*2 a su monto total
+                    jugadores[i].setMonto(jugadores[i].getMonto() + (apuesta * 2));
+                    //  Reiniciar cartas para nuevo juego
+                    jugadores[i].nuevoJuego();
+                    mesa->nuevoPuntaje();
+                    //  Mensaje ganador
+                    auxMsj = "El puntaje de la mesa es de: " + to_string(puntajeMesa) +
+                             "\nEl puntaje del jugador es de: " + to_string(puntajeJugador) +
+                             "\n> Jugador " + to_string(jugadores[i].getId()) + " ha ganado!" +
+                             "\nSu nuevo monto es de: " + to_string(jugadores[i].getMonto()) +
+                             "\n[---- NUEVO JUEGO ----]\n";
+                    auxMsj += jugadores[i].imprimirCartas();
+                }
+                else
+                {
+                    //  Sumar lo apostado a su monto total
+                    jugadores[i].setMonto(jugadores[i].getMonto() - apuesta);
+                    //  Reiniciar cartas para nuevo juego
+                    jugadores[i].nuevoJuego();
+                    mesa->nuevoPuntaje();
+                    //  Mensaje ganador
+                    auxMsj = "El puntaje de la mesa es de: " + to_string(puntajeMesa) +
+                             "\nEl puntaje del jugador es de: " + to_string(puntajeJugador) +
+                             "\n>Jugador " + to_string(jugadores[i].getId()) + " ha perdido por tener menos puntos que la mesa :(" +
+                             "\nSu nuevo monto es de: " + to_string(jugadores[i].getMonto()) +
+                             "\n[---- NUEVO JUEGO ----]\n";
+                    auxMsj += jugadores[i].imprimirCartas();
+                }
+            }
+
+            if (jugadores[i].getMonto() < 0 || opcion == 9)
+            {
+                auxMsj = to_string(9);
+                enviarMensaje(fd2[i], pid, mutex[i], msg, auxMsj);
+                break;
+            }
         }
     }
 
