8b26454ea6496b73b28164d735d2150daf28b5a9
Nicolas Chirino <nicolas.brch@gmail.com>
2023-06-08 22:53:18 -0400

    Crear archivo y directorio

--- tarea-3/main.cpp
+++ tarea-3/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <fstream>
+#include <sys/stat.h>
 
 using namespace std;
+
+bool crearArchivo(const string& nombreArchivo) {
+    ofstream archivo(nombreArchivo);
+    if (archivo) {
+        archivo.close();
+        return true;
+    } else {
+        cout << "Error: No se pudo crear el archivo." << endl;
+        return false;
+    }
+}
+
+bool crearDirectorio(const string& dirname) {
+    int result = mkdir(dirname.c_str(), 0777);
+    if (result == 0) {
+        return true;
+    } else {
+        cout << "Error: No se pudo crear el directorio." << endl;
+        return false;
+    }
+}
 
 int main(){
 
