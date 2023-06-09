10c8dfe14a572fef8d7a405fcd9106385d4ca4b7
Nicolas Chirino <nicolas.brch@gmail.com>
2023-06-08 23:04:55 -0400

    Gestionar permisos y listar archivos

--- tarea-3/main.cpp
+++ tarea-3/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <sys/stat.h>
+#include <dirent.h>
+#include <cstring>
 
 using namespace std;
 
@@ -15,13 +17,70 @@ bool crearArchivo(const string& nombreArchivo) {
     }
 }
 
-bool crearDirectorio(const string& dirname) {
-    int result = mkdir(dirname.c_str(), 0777);
-    if (result == 0) {
+bool crearDirectorio(const string& nombreDirectorio) {
+    int directorio = mkdir(nombreDirectorio.c_str(), 0777);
+    if (directorio == 0) {
         return true;
     } else {
         cout << "Error: No se pudo crear el directorio." << endl;
         return false;
+    }
+}
+
+bool gestionarPermisos(const string& path, mode_t permisos) {
+    int gestionar = chmod(path.c_str(), permisos);
+    if (gestionar == 0) {
+        return true;
+    } else {
+        cout << "Error: No se pudieron cambiar los permisos." << endl;
+        return false;
+    }
+}
+
+void listarInodos(const string& path) {
+    DIR* directorio = opendir(path.c_str());
+    if (directorio) {
+        struct dirent* archivo;
+        while ((archivo = readdir(directorio)) != nullptr) {
+            string nombreArchivo = archivo->d_name;
+            string rutaArchivo = path + "/" + nombreArchivo;
+
+            struct stat inodo;
+            if (stat(rutaArchivo.c_str(), &inodo) == 0) {
+                cout << archivo->d_ino << "\t" << nombreArchivo << endl;
+            }
+        }
+
+        closedir(directorio);
+    } else {
+        cout << "Error: No se pudo abrir el directorio." << endl;
+    }
+}
+
+void listarRecursivo(const string& path) {
+    DIR* directorio = opendir(path.c_str());
+    if (directorio) {
+        struct dirent* archivo;
+        while ((archivo = readdir(directorio)) != nullptr) {
+            if (strcmp(archivo->d_name, ".") != 0 && strcmp(archivo->d_name, "..") != 0) {
+                string nombreArchivo = archivo->d_name;
+                string rutaArchivo = path + "/" + nombreArchivo;
+
+                struct stat inodo;
+                if (stat(rutaArchivo.c_str(), &inodo) == 0) {
+                    if (S_ISDIR(inodo.st_mode)) {
+                        cout << rutaArchivo << ":" << endl;
+                        listarRecursivo(rutaArchivo);
+                    } else {
+                        cout << rutaArchivo << endl;
+                    }
+                }
+            }
+        }
+
+        closedir(directorio);
+    } else {
+        cout << "Error: No se pudo abrir el directorio." << endl;
     }
 }
 
